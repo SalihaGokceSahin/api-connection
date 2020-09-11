16b26412a4e83af049a653733cace6850670bf6a
saliha <salihagokce91@gmail.com>
2020-09-11 14:00:37 +0300

    first api commit.json database

---
+++ WebApi/Controllers/citiesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class citiesController : ApiController
+    {
+        private string[] cities = new string[] { "antalya", "adana", "mersin", "istanbul" };
+        public string[] Get()
+        {
+            return cities;
+        }
+        public string Get(int id)
+        {
+            return cities[id];
+        }
+    }
+}
